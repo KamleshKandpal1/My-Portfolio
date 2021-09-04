3826ccf0ef5b231cb9de2b7e1c05fcdb0ddcf29b
kk-op2 <85089825+kk-op2@users.noreply.github.com>
2021-09-05 01:43:44 +0530

    first commit

--- assets/JS File/script.js
+++ assets/JS File/script.js
@@ -18,7 +18,7 @@ $(document).ready(function () {
   });
   // typing animation script
   var typed = new Typed(".typing", {
-    strings: ["Student", "Developer", "Designer", "Freelancer"],
+    strings: ["Student", "Learner"], //"Developer", //"Designer", //"Freelancer"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
